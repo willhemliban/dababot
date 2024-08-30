e773578c2624b1b9e0d9c9af18f27335aa508887
Willhem Liban <48018918+willhemliban@users.noreply.github.com>
2024-08-30 11:38:51 +0200

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,9 +1,9 @@
 # personal details 
 # Obtain Keys under 'Keys and Tokens'at https://developer.twitter.com/en/apps/[APP #] 
 API_KEY ="ioZ9yIUAgZamo82zjJWG6t0D6" 
-API_SECRET ="zasn863oBnbvaO7fYm6CGmkXJVZ6bKnaS5JuGCUnnIUxZNT8bU" 
+API_SECRET ="" 
 ACCESS_TOKEN ="1374876510411182082-XwybKmNlbjlIrxq7aLxRwvO8mzN0DP"
-ACCESS_TOKEN_SECRET ="5JtO0AZlWgxOaIEgHL9DDtIhnuShlNP8e06RMMJbFNJ0M"
+ACCESS_TOKEN_SECRET =""
 
 ACCOUNT_ID = "1250155034232475650"
 ACCOUNT_NAME = "20437446"
